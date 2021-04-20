1d585256790117478dfad6b01c66a7e5f6813d3d
mehdi medjdoub <mehdimedjdoub@macbook-pro-de-mehdi.home>
2021-04-20 14:30:35 +0200

    enable gmail host

--- src/services/nodemailer/transport.ts
+++ src/services/nodemailer/transport.ts
@@ -1,12 +1,23 @@
 import nodemailer from 'nodemailer'
 
+// const transporter = nodemailer.createTransport({
+//     host: process.env.MAIL_HOST,
+//     port: Number(process.env.MAIL_PORT),
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//         user: process.env.MAIL_USERNAME,
+//         pass: process.env.MAIL_PASSWORD
+//     },
+//     debug: true,
+//     logger: true
+// });
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT),
+    host: 'smtp.gmail.com',
+    port: 465,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD
+        user: 'supercompletive@gmail.com',
+        pass: 'completive123!'
     },
     debug: true,
     logger: true
